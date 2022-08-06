2f2a828d7e90e8402217e46882d6d4a7ead722b9
Will Lovell <willard.lovell@icloud.com>
2022-08-06 16:26:36 -0600

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './COMPONENTS/HomePage';
 import NavBar from './COMPONENTS/NavBar';
 import ProductCard from './COMPONENTS/products/ProductCard';
@@ -8,15 +9,16 @@ import ShoppingCart from './COMPONENTS/ShoppingCart';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <Router>
       <NavBar />
-      <ProductCard />
-      <ProductList />
-      <ShoppingCart />
-
-
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductCard />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        
+          </Routes>
+        </Router>
   );
 }
 
--- src/COMPONENTS/NavBar.jsx
+++ src/COMPONENTS/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-	<div>NavBar</div>
+	<div>
+    <ul>
+      
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/product">Products</Link></li>
+      <li><Link to="/cart">Shopping Cart</Link></li>
+
+      
+    </ul>
+
+
+  </div>
   )
 }
 
